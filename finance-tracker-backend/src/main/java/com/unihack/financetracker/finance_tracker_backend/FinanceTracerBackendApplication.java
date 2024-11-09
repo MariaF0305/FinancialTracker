7e80f721adb47e7269b7d93134cac6200c7a3163
MariaF03055 <maria.fintineanu04@e-uvt.ro>
2024-11-09 03:42:48 +0200

    Packages were created

---
+++ finance-tracker-backend/src/main/java/com/unihack/financetracker/finance_tracker_backend/FinanceTracerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unihack.financetracker.finance_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTracerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTracerBackendApplication.class, args);
+	}
+
+}
---
+++ finance-tracker-backend/src/test/java/com/unihack/financetracker/finance_tracker_backend/FinanceTracerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unihack.financetracker.finance_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTracerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
